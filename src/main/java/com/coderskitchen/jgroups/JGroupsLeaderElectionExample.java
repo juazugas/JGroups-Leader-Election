3eb4f193ef6d3500b21880e7f1d5769ec183bb4e
jzuriaga <jzuriaga@redhat.com>
2019-07-03 21:09:47 +0200

    Play with Jgroups channel and communications.

--- src/main/java/com/coderskitchen/jgroups/JGroupsLeaderElectionExample.java
+++ src/main/java/com/coderskitchen/jgroups/JGroupsLeaderElectionExample.java
@@ -1,19 +1,31 @@
 package com.coderskitchen.jgroups;
 
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import org.jgroups.Address;
 import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
 
-public class JGroupsLeaderElectionExample {
+public class JGroupsLeaderElectionExample extends ReceiverAdapter {
 
   private static final int MAX_ROUNDS = 1_000;
   private static final int SLEEP_TIME_IN_MILLIS = 1000;
+  
+  	private int leadperiod;
+	private Address currentAddress;
+	private Predicate<Address> sameAddress = m -> m.equals(currentAddress);
 
   public static void main(String[] args) throws Exception {
+	  JGroupsLeaderElectionExample member = new JGroupsLeaderElectionExample();
     JChannel channel = new JChannel();
     channel.connect("The Test Cluster");
+    channel.setReceiver(member);
+    member.currentAddress = channel.getAddress();
     for (int round = 0; round < MAX_ROUNDS; round++) {
-      checkLeaderStatus(channel);
+      member.checkLeaderStatus(channel);
       sleep();
     }
 
@@ -28,14 +40,51 @@ public class JGroupsLeaderElectionExample {
     }
   }
 
-  private static void checkLeaderStatus(JChannel channel) {
+  private void checkLeaderStatus(JChannel channel) {
+	
     View view = channel.getView();
     Address address = view.getMembers()
                           .get(0);
-    if (address.equals(channel.getAddress())) {
-      System.out.println("I'm (" + channel.getAddress() + ") the leader");
+    if (address.equals(currentAddress)) {
+      leadperiod++;
+      System.out.print("I'm (" + currentAddress + ") the leader for " + leadperiod + " cycles.");
+      String members = getMemberList(view, currentAddress);
+      System.out.println("other members : " + members);
+      if (leadperiod % 4 == 0) {
+    	  checkCitizens(channel, view);
+      }
     } else {
-      System.out.println("I'm (" + channel.getAddress() + ") not the leader");
+      System.out.println("I'm (" + currentAddress + ") not the leader");
+      leadperiod = 0;
     }
   }
+  
+  	@Override
+	public void receive(Message msg) {
+  		System.out.println("Message from :" +msg.getSrc() + " : " + new String(msg.getBuffer()));
+	}
+
+	private void checkCitizens(JChannel channel, View view) {
+		view.getMembers().stream()
+			.filter(sameAddress.negate())
+			.map(m -> new Message(m, currentAddress,  "Greets from your master "+channel.getAddressAsString()))
+			.forEach(m -> sendMessage(channel, m));
+	}
+	
+	private void sendMessage (JChannel channel, Message message) {
+		try {
+			channel.send(message);
+		} catch (Exception e) {
+			System.err.println("citizen not located " + message.getDest().toString());
+			e.printStackTrace();
+		}
+	}
+	
+	private String getMemberList(View view, Address currentAddress) {
+		
+		return view.getMembers().stream()
+				  .filter(sameAddress.negate())
+				  .map(Address::toString)
+				  .collect(Collectors.joining(", "));
+	}
 }
